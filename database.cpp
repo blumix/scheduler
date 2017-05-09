1c670808d8404722ea64c1cd0fb5fe00d75e063b
homesick94 <kt2608@gmail.com>
2017-05-09 21:14:27 +0300

    NEW: mainwindow: added solver part (2)

--- database.cpp
+++ database.cpp
@@ -7,4 +7,7 @@ database::database()
   m_teachers = std::make_unique<index_table<teacher_profile>> ();
 
   m_result_schedule.resize (common_defines::all_timeslots);
+
+  for (int idx = 0; idx < common_defines::all_timeslots; idx++)
+    m_result_schedule[idx] = std::vector<int> (m_groups->get_ids ().size (), -1);
 }
